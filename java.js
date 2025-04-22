6723fcfbf6753b899c171cd3999731329013100a
Lou :) <136341659+louloou@users.noreply.github.com>
2025-04-22 18:39:21 -0300

    cambiosJava

---
+++ java.js
@@ -0,0 +1,10 @@
+let boton = document.querySelector('#modoOscuro')
+let body = document.querySelector('body')
+
+boton.onclick = function() {
+    
+    body.classList.toggle('dark')
+}
+    
+    
+    
