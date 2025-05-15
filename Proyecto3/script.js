6676f922344bf24265f12b7428f60b6731327126
alumno <alumno@Instituto.local>
2025-05-15 11:11:19 -0300

    u

--- Proyecto3/script.js
+++ Proyecto3/script.js
@@ -1,7 +1,7 @@
-//1
+//1 Creación de un arreglo
 let colores = ['Azul', 'Violeta', 'Blanco','Turquesa', 'Rosa'];
 
-//2
+//2  Acceso a elementos
 console.log(colores);
 
 let primero = colores[0];
@@ -10,27 +10,28 @@ let ultimo = colores[colores.length - 1];
 console.log(primero);
 console.log(ultimo);
 
-//3
+//3  Tamaño del arreglo:
+
 console.log(colores.length);
 
-//4
+//4 Agregar al final
 colores.push("Naranja");
 
-//5
+//5 Agregar al inicio
 colores.unshift("Rojo");
 console.log(colores);
 
-//6
+//6 Eliminar del final
 colores.pop();
 
-//7
+//7 Eliminar del principio
 colores.shift();
 
-//8
+//8 Reemplazo de un valor
 colores[2] = "Amarillo";
 console.log(colores);
 
-//9
+//9 Recorrido del arreglo
 
 for (let i = 0; i < colores.length; i++) {
     console.log(colores[i]);
@@ -38,7 +39,7 @@ for (let i = 0; i < colores.length; i++) {
 }
 
 
-//10
+//10 Desafío creativo
 
 let nuevosColores = []
 
