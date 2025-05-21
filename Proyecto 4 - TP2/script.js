48b918e49e7fd11dd1b128b78f09cd566d775236
alumno <alumno@Instituto.local>
2025-05-21 08:16:08 -0300

    tp2

---
+++ Proyecto 4 - TP2/script.js
@@ -0,0 +1,56 @@
+// Ejercicio 1
+//1 Creación de un arreglo
+let frutas = ['Frutilla', 'Manzana', 'Mandarina'];
+
+//2 Agrega una fruta al final del arreglo
+frutas.push('Ananá');
+
+//3 Agrega una fruta al principio del arreglo
+frutas.unshift('Sandía');
+
+//4 Elimina la última fruta
+frutas.pop();
+
+//5 Elimina la primera fruta
+frutas.shift();
+
+//6 Muestra el contenido del arreglo con console.log
+console.log(frutas);
+
+// Ejercicio 2
+//1 Crea un arreglo de nombres
+let nombreAlumnos = ['Sasha', 'Martina', 'María'];
+
+//2 Muestra cuántos nombres hay en el arreglo usando .length
+console.log(nombreAlumnos.length);
+
+//3 Usa push para agregar dos nombres 
+frutas.push('Fabiana', 'Sandra'); 
+
+//4 Vuelve a mostrar cuántos nombres hay
+console.log(nombreAlumnos.length);
+
+// Ejercicio 3
+//1 Crea un arreglo de números
+let numeros = ['13', '22', '29'];
+
+//2 Usa un ciclo for para mostrar cada número por consola
+
+
+//Ejercicio 4
+//1 Crea un arreglo de números
+let numerosPares = ['2', '4', '6', '8'];
+
+//2 Usa un ciclo for para sumar todos los elementos
+
+//3 Muestra el resultado con console.log
+console.log(numerosPares.length);
+
+//Ejercicio 5
+//1 Pide al usuario que escriba cinco palabras (una por una con prompt)
+
+//2 Guarda cada palabra en un arreglo usando push
+
+//3 Muestra el contenido del arreglo
+
+
