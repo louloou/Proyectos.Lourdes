71480b395b33ffab41d4c6e034959689b9be7d4a
alumno <alumno@Instituto.local>
2025-05-22 11:03:11 -0300

    finalActividad

--- Proyecto 4 - TP2/script.js
+++ Proyecto 4 - TP2/script.js
@@ -35,22 +35,31 @@ console.log(nombreAlumnos.length);
 let numeros = ['13', '22', '29'];
 
 //2 Usa un ciclo for para mostrar cada número por consola
-
+for (let i = 0; i < numeros.length; i++) {
+    console.log(numeros[i]);
+}
 
 //Ejercicio 4
 //1 Crea un arreglo de números
 let numerosPares = ['2', '4', '6', '8'];
 
 //2 Usa un ciclo for para sumar todos los elementos
-
+let suma = 0;
+for (let i = 0; i < numerosPares.length; i++) {
+    suma += parseInt(numerosPares[i]); // revisar en casa
+}
 //3 Muestra el resultado con console.log
-console.log(numerosPares.length);
+console.log("La suma es:", suma);
 
 //Ejercicio 5
 //1 Pide al usuario que escriba cinco palabras (una por una con prompt)
-
-//2 Guarda cada palabra en un arreglo usando push
+let palabras = [];
+for (let i = 0; i < 5; i++) {
+    let palabra = prompt(`Escribe la palabra #${i + 1}:`);
+    
+    //2 Guarda cada palabra en un arreglo usando push
+    palabras.push(palabra);
+}
 
 //3 Muestra el contenido del arreglo
-
-
+console.log("Las palabras ingresadas son:", palabras);
