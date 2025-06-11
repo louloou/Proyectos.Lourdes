cbf312d14bb8aafa9b2d902eaad1a10c089d6db3
alumno <alumno@Instituto.local>
2025-06-11 08:21:01 -0300

    clase11Junio

---
+++ Proyecto 5 - TP3/script.js
@@ -0,0 +1,37 @@
+// Ejercicio 1: Mostrar todos los elementos
+
+let animales = ["perro", "gato", "conejo", "loro"];
+
+for (let i = 0; i < animales.length; i++) {
+    console.log(animales[i]); 
+}
+
+//Ejercicio 2: Mostrar sólo elementos en posiciones pares
+
+//Ejercicio 3: Sumar los números del arreglo
+let numeros = [4, 7, 2, 9, 5];
+let suma = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+  suma += numeros[i];
+}
+console.log("La suma es:", suma); 
+
+//Ejercicio 4: Multiplicar todos los elementos por 2
+let datos = [3, 6, 1, 8];
+let multiplicados = []; 
+
+for (let i = 0; i < datos.length; i++) {
+  multiplicados.push(datos[i] * 2);  
+}
+console.log(multiplicados);
+
+// Ejercicio 5: Buscar un nombre
+let alumnos = ["Sofía", "Carlos", "Bruno", "Lucía", "Carla"];
+
+//  Ejercicio 6: Reemplazar todos los ceros por cien
+let valores = [5, 0, 8, 0, 2];
+
+// Ejercicio 7: Promedio de notas
+let notas = [8, 7, 9, 10, 6];
+
