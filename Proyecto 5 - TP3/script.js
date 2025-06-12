6a33c12ed14ca470156d34bd8593fc4ba41d70f4
Lou :) <136341659+louloou@users.noreply.github.com>
2025-06-11 21:23:48 -0300

    uptade script

--- Proyecto 5 - TP3/script.js
+++ Proyecto 5 - TP3/script.js
@@ -8,6 +8,12 @@ for (let i = 0; i < animales.length; i++) {
 
 //Ejercicio 2: Mostrar sólo elementos en posiciones pares
 
+for (let i = 0; i < animales.length; i++) {
+  if (i % 2 === 0) {
+    console.log(animales[i]);
+  }
+}
+
 //Ejercicio 3: Sumar los números del arreglo
 let numeros = [4, 7, 2, 9, 5];
 let suma = 0;
@@ -28,10 +34,32 @@ console.log(multiplicados);
 
 // Ejercicio 5: Buscar un nombre
 let alumnos = ["Sofía", "Carlos", "Bruno", "Lucía", "Carla"];
+let nombre = prompt("Ingresá un nombre:");
+
+if (alumnos.includes(nombre)) {  // preguntar por includes
+  console.log("Se encontró el nombre.");
+} else {
+  console.log("No se encontró el nombre.");
+}
 
 //  Ejercicio 6: Reemplazar todos los ceros por cien
 let valores = [5, 0, 8, 0, 2];
 
+for (let i = 0; i < valores.length; i++) {
+  if (valores[i] === 0) {
+    valores[i] = 100;
+  }
+}
+
+console.log(valores);
+
 // Ejercicio 7: Promedio de notas
 let notas = [8, 7, 9, 10, 6];
+let sumaPromedio = 0;
 
+for (let i = 0; i < notas.length; i++) {
+  sumaPromedio += notas[i];
+}
+
+let promedio = sumaPromedio / notas.length;
+console.log("El promedio es " + promedio);
