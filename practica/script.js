946c6fcefb36c4a0947bfb2d5a4a82fad0c8dbd0
alumno <alumno@Instituto.local>
2025-07-31 11:15:49 -0300

    uptade

--- practica/script.js
+++ practica/script.js
@@ -3,19 +3,29 @@
 let estudiantes = [{
     nombre: 'Sasha',
     apellido: 'Cisneros',
-    edad: 16
+    edad: 16, 
+    notaExamen: 10
 },
 {
     nombre: 'Martina',
     apellido: 'Semper',
-    edad: 16
+    edad: 16, 
+    notaExamen: 8
 }, {  
     nombre: 'Martina',
     apellido: 'Ferrando',
-    edad: 16
+    edad: 16,
+    notaExamen: 8
+},
+{  
+    nombre: 'Camilo',
+    apellido: 'Yorgovan',
+    edad: 16,
+    notaExamen: 5
 }
 ]
 
 for (let index = 0; index < estudiantes.length; index++) {
     // Mostrar todos los elementos del arreglo
-   console.log(estudiantes[index].apellido + ' '+ (estudiantes[index].nombre)) }
+   console.log(estudiantes[index].nombre + ' ' + estudiantes[index].apellido + '  Su nota es: '+ estudiantes[index].notaExamen)} 
+
