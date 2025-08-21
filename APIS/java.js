624f5c2d1cc481902e1bab407f08ec81c277f24b
alumno <alumno@Instituto.local>
2025-08-21 11:18:27 -0300

    nose

---
+++ APIS/java.js
@@ -0,0 +1,13 @@
+let titulo = document.querySelector('#titulo')
+let imagen = document.querySelector('#imagen')
+let boton = document.querySelector('#boton')
+
+boton.onclick = function (){
+    fetch('https://pokeapi.co/api/v2/pokemon-form/157/')
+    .then(respuesta => respuesta.json())
+    .then (datos => {
+        console.log(datos)
+        titulo.textContent = datos.name
+        imagen.src = datos.sprites.font_shiny 
+    })
+}
