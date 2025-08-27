b4e14ffd45ee47a62ac1055892702910a0bd18b1
Lou :) <136341659+louloou@users.noreply.github.com>
2025-08-27 19:57:31 -0300

    cambiosPokeAPI

--- APIS/java.js
+++ APIS/java.js
@@ -1,3 +1,4 @@
+/* -Clase anterior-
 let titulo = document.querySelector('#titulo')
 let imagen = document.querySelector('#imagen')
 let boton = document.querySelector('#boton')
@@ -11,4 +12,21 @@ boton.onclick = function (){
         imagen.src = datos.sprites.front_shiny
     })
 } 
+*/ 
 
+function cargarPokemon(id, imgId, nameId) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then(respuesta => respuesta.json())
+    .then(data => {
+      document.querySelector(imgId).src = data.sprites.front_default;
+      document.querySelector(nameId).textContent = data.name;
+    })
+    .catch(err => console.error(err));
+}
+
+cargarPokemon(1, '#pokemon1', '#nombre1');
+cargarPokemon(2, '#pokemon2', '#nombre2');
+cargarPokemon(3, '#pokemon3', '#nombre3');
+cargarPokemon(4, '#pokemon4', '#nombre4');
+cargarPokemon(5, '#pokemon5', '#nombre5');
+cargarPokemon(6, '#pokemon6', '#nombre6');
